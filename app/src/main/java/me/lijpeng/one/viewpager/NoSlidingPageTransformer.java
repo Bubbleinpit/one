c98f0a2d46e5e637dcc2f277e0bdf8d04fe64381
Bubbleinpit <coke909@live.com>
2017-05-30 15:33:03 +0800

    添加一些注释

--- app/src/main/java/me/lijpeng/one/viewpager/NoSlidingPageTransformer.java
+++ app/src/main/java/me/lijpeng/one/viewpager/NoSlidingPageTransformer.java
@@ -3,6 +3,10 @@ package me.lijpeng.one.viewpager;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
+/*
+*设置Fragment切换时的动画为淡入淡出
+*/
+
 public class NoSlidingPageTransformer implements ViewPager.PageTransformer {
     private static final float MIN_ALPHA = 0.0f;    //最小透明度
 
@@ -27,9 +31,9 @@ public class NoSlidingPageTransformer implements ViewPager.PageTransformer {
             //透明度改变Log
             view.setAlpha(alphaFactor);
             if (alphaFactor == 0)
-                view.setVisibility(View.INVISIBLE);
+                view.setVisibility(View.INVISIBLE);     //页面不在当前界面显示，则使其Invisible，这句是为了解决Fragment对上下滑动事件监听的错乱，暂不知原因
             else if (view.getVisibility() == View.INVISIBLE)
-                view.setVisibility(View.VISIBLE);
+                view.setVisibility(View.VISIBLE);       //页面在当前界面显示，则使其Visible，这句是为了解决Fragment对上下滑动事件监听的错乱，暂不知原因
         } else { // (1,+Infinity]
             // This page is way off-screen to the right.    出了右边屏幕
             view.setAlpha(0);
